25cc4ec54fb2caa4a78445542ccb9f1f373a3ec9
slambros <92240247+slambros@users.noreply.github.com>
2022-04-20 22:48:22 -0700

    Add files via upload

---
+++ run_everything.m
@@ -0,0 +1,27 @@
+clear %clear all variables
+close all %close all figs
+%%
+addpath 'C:\Users\sarah\Desktop\Project\Files\Model_codes' 
+models 
+% %%
+filter_by_month
+average_s
+variable_averages
+plot_average %can include
+%%
+% stdev_s
+% variable_stds
+% plot_std
+%plot_more_stats_tran 
+%plot_more_stats_sub
+% percent_pos_transub
+diff_divided_transub
+% best_and_worst_boxplot_tran
+%best_and_worst_boxplot_sub
+% top_and_bottom_percent_tran
+% top_and_bottom_percent_sub
+most_negative_sub
+most_negative_tran
+top_and_bottom_percent_sub_normalized %FIX
+top_and_bottom_percent_tran_normalized %FIX
+ave_tran_sub_graph
